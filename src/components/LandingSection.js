1f12bc3ca5a22537791f2c0f3f6acd1e33644e5d
Anjana Padmanabhan <anjnambiar@gmail.com>
2023-09-29 16:26:29 -0400

    initial commit

---
+++ src/components/LandingSection.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Anjana!";
+const bio1 = "A frontend developer";
+const bio2 = "Skills : JavaScript, React, HTML/CSS, Figma";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+   // isDarkBackground
+    backgroundColor=  "#FFFFFF"//"#2A4365"
+  >
+    <VStack>
+    {/* <Avatar src="https://i.pravatar.cc/150?img=7"></Avatar> */}
+    <Heading as='h2' size='sm'>{greeting}</Heading>
+    <br></br>
+    <Heading as='h2' size='xl'>{bio1}</Heading>
+    <Heading as='h2' size='l'>{bio2}</Heading>
+    </VStack>
+  </FullScreenSection>
+);
+
+export default LandingSection;
